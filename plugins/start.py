2e238ee38974f4a9472ec5df32d61f29ffd777e8
Bot-Bucket <163278950+Bot-Bucket@users.noreply.github.com>
2024-05-11 09:38:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -137,26 +137,27 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = '• ɴᴏᴡ ᴄʟɪᴄᴋ ʜᴇʀᴇ •',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
         )
     except IndexError:
         pass
-        
-        await message.reply_text(
-            f"Hello {message.from_user.first_name},\n"
-            f"last_name = {message.from_user.last_name},\n"
-            f"username = {'@' + message.from_user.username if message.from_user.username else 'None'},\n"
-            f"mention = {message.from_user.mention},\n"
-            f"id = {message.from_user.id}",
-            reply_markup=InlineKeyboardMarkup(buttons),
-            quote=True,
-            disable_web_page_preview=True
-        )
-    except Exception as e:
-        print(e)
+
+    await message.reply(
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
+    )
+
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
